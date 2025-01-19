e64af9f4f4c48a461364f78ecbadeb256a78c421
Rajanikant04 <rajanikantsinghnitr@gmail.com>
2025-01-19 15:44:55 +0530

    implement user authentication with signup, login, logout, and profile retrieval; add middleware for route protection and JWT token handling

--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,23 @@ import dotenv from 'dotenv';
 
 import authRoutes from './routes/auth.route.js';
 import connectMongoDB from './db/connectMongoDB.js';
+import cookieParser from 'cookie-parser';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(express.json({ limit: "5mb" })); // to parse req.body
+// limit shouldn't be too high to prevent DOS
+app.use(express.urlencoded({ extended: true })); // to parse form data(urlencoded)
+// these parsers should written before the file path call (large problematic this one)
+
+app.use(cookieParser());
+
 app.use('/api/auth', authRoutes);
+
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
   connectMongoDB();
