61813499e5621909a6d82a3a25f1ec94b1a28955
Rajanikant04 <rajanikantsinghnitr@gmail.com>
2025-01-26 21:11:00 +0530

    error resolved

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,6 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -19,7 +18,6 @@ createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
         <App />
-        <ReactQueryDevtools />
       </QueryClientProvider>
     </BrowserRouter>
   </StrictMode>,
