a312af9612f3c5e95c289630a0eb34d89abe2434
Rajanikant04 <rajanikantsinghnitr@gmail.com>
2025-01-28 23:44:23 +0530

    profile page posts and user info fetched

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -6,13 +6,12 @@ import Notification from "../models/notification.model.js";
 
 export const getUserProfile = async (req,res) =>{
     const {username} = req.params;
-
     try {
         const user = await User.findOne({username}).select("-password");
         if(!user) {
             return res.status(404).json({error: "User not found"});
         }
-        res.status(200).json({user});
+        res.status(200).json(user); // send user data directly... {user} don't return like this
     } catch (error) {
         console.log("Error in getUserProfile: ",error.message);
         res.status(500).json({error: error.message});
--- frontend/src/pages/profile/ProfilePage.jsx
+++ frontend/src/pages/profile/ProfilePage.jsx
@@ -1,5 +1,6 @@
-import { useRef, useState } from "react";
-import { Link } from "react-router-dom";
+import { useEffect, useRef, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
 
 import Posts from "../../components/common/Posts";
 import ProfileHeaderSkeleton from "../../components/skeletons/ProfileHeaderSkeleton";
@@ -11,7 +12,7 @@ import { FaArrowLeft } from "react-icons/fa6";
 import { IoCalendarOutline } from "react-icons/io5";
 import { FaLink } from "react-icons/fa";
 import { MdEdit } from "react-icons/md";
-import { useQuery } from "@tanstack/react-query";
+import { formatMemberSinceDate } from "../../utils/date";
 
 const ProfilePage = () => {
 
@@ -22,20 +23,33 @@ const ProfilePage = () => {
   const coverImgRef = useRef(null);
   const profileImgRef = useRef(null);
 
-  const isLoading = false;
-  const isMyProfile = true;
+  const {username} = useParams();
 
-  const user = {
-    _id: "1",
-    fullName: "John Doe",
-    username: "johndoe",
-    profileImg: "/avatars/boy2.png",
-    coverImg: "/cover.png",
-    bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    link: "https://youtube.com/@asaprogrammer_",
-    following: ["1", "2", "3"],
-    followers: ["1", "2", "3"],
-  };
+  const isMyProfile = true;
+  
+  const {data:user, isLoading, refetch, isRefetching} = useQuery({
+    queryKey: ["userProfile"],
+    queryFn: async () => {
+      try {
+        const res = await fetch(`/api/users/profile/${username}`);
+        const data = await res.json();
+        if(!res.ok) {
+          throw new Error(data.error || "Something went wrong");
+        }
+        return data;
+      } catch (error) {
+        throw new Error(error);
+      }
+    }
+  })
+  
+
+  // TODO: here is a issue of user.. we are not getting the user directly.. we are getting the user object and inside that object there is one more user object and we can access our data through the inside user object... it need to be modified
+  // console.log((user));
+  // const createdAt = user.createdAt;
+  // console.log(user.user.createdAt)
+  
+  const memberSinceDate = formatMemberSinceDate(user?.createdAt)
 
   const handleImgChange = (e, state) => {
     const file = e.target.files[0];
@@ -49,16 +63,22 @@ const ProfilePage = () => {
     }
   };
 
+  useEffect(() => {
+    refetch()
+  }, [username,refetch])
+  
+
+
   return (
     <>
       <div className="flex-[4_4_0]  border-r border-gray-700 min-h-screen ">
         {/* HEADER */}
-        {isLoading && <ProfileHeaderSkeleton />}
-        {!isLoading && !user && (
+        {isLoading && isRefetching && <ProfileHeaderSkeleton />}
+        {!isLoading && !isRefetching && !user && (
           <p className="text-center text-lg mt-4">User not found</p>
         )}
         <div className="flex flex-col">
-          {!isLoading && user && (
+          {!isLoading && !isRefetching && user && (
             <>
               <div className="flex gap-10 px-4 py-2 items-center">
                 <Link to="/">
@@ -170,7 +190,7 @@ const ProfilePage = () => {
                   <div className="flex gap-2 items-center">
                     <IoCalendarOutline className="w-4 h-4 text-slate-500" />
                     <span className="text-sm text-slate-500">
-                      Joined July 2021
+                      {memberSinceDate}
                     </span>
                   </div>
                 </div>
@@ -212,7 +232,7 @@ const ProfilePage = () => {
             </>
           )}
 
-          <Posts />
+          <Posts feedType={feedType} username={username} userId={user?._id} />
         </div>
       </div>
     </>
