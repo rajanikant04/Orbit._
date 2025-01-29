09383caa61de1eff2f65c9a436d5deeee53928a4
Rajanikant04 <rajanikantsinghnitr@gmail.com>
2025-01-29 18:37:29 +0530

    update user profile in edit profile modal

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -7,7 +7,7 @@ const router = express.Router();
 router.get("/profile/:username",protectRoute, getUserProfile)
 router.get("/suggested", protectRoute, getSuggestedUser)
 router.get("/follow/:id", protectRoute, followUnfollowUser)
-router.get("/update", protectRoute, updateUser)
+router.post("/update", protectRoute, updateUser)
 
 
 export default router;
--- frontend/src/pages/profile/ProfilePage.jsx
+++ frontend/src/pages/profile/ProfilePage.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useRef, useState } from "react";
 import { Link, useParams } from "react-router-dom";
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+import toast from "react-hot-toast";
 
 import Posts from "../../components/common/Posts";
 import ProfileHeaderSkeleton from "../../components/skeletons/ProfileHeaderSkeleton";
@@ -14,6 +15,8 @@ import { FaLink } from "react-icons/fa";
 import { MdEdit } from "react-icons/md";
 import { formatMemberSinceDate } from "../../utils/date";
 
+import useFollow from "../../hooks/useFollow";
+
 const ProfilePage = () => {
 
   const [coverImg, setCoverImg] = useState(null);
@@ -25,8 +28,12 @@ const ProfilePage = () => {
 
   const {username} = useParams();
 
-  const isMyProfile = true;
-  
+  // const isMyProfile = true;
+  const {follow,isPending} = useFollow();
+  const queryClient = useQueryClient();
+  const {data:authUser} = useQuery({queryKey:["authUser"]})
+
+
   const {data:user, isLoading, refetch, isRefetching} = useQuery({
     queryKey: ["userProfile"],
     queryFn: async () => {
@@ -41,10 +48,13 @@ const ProfilePage = () => {
         throw new Error(error);
       }
     }
-  })
+  });
+
   
   
+  const isMyProfile = authUser._id === user?._id;
   const memberSinceDate = formatMemberSinceDate(user?.createdAt)
+  const amIFollowing = authUser?.following.includes(user?._id);
 
   const handleImgChange = (e, state) => {
     const file = e.target.files[0];
@@ -138,21 +148,23 @@ const ProfilePage = () => {
                 </div>
               </div>
               <div className="flex justify-end px-4 mt-5">
-                {isMyProfile && <EditProfileModal />}
+                {isMyProfile && <EditProfileModal authUser={authUser}/>}
                 {!isMyProfile && (
                   <button
                     className="btn btn-outline rounded-full btn-sm"
-                    onClick={() => alert("Followed successfully")}
+                    onClick={() => follow(user?._id)}
                   >
-                    Follow
+                    {isPending && "Loading..."}
+                    {!isPending && amIFollowing && "Unfollow"}
+                    {!isPending && !amIFollowing && "Follow"}
                   </button>
                 )}
                 {(coverImg || profileImg) && (
                   <button
                     className="btn btn-primary rounded-full btn-sm text-white px-4 ml-2"
-                    onClick={() => alert("Profile updated successfully")}
+                    onClick={() => updateProfile()}
                   >
-                    Update
+                    {isUpdatingProfile ? "Updating..." : "Update"}
                   </button>
                 )}
               </div>
@@ -172,7 +184,7 @@ const ProfilePage = () => {
                       <>
                         <FaLink className="w-3 h-3 text-slate-500" />
                         <a
-                          href="https://youtube.com/@asaprogrammer_"
+                          href={authUser.link}
                           target="_blank"
                           rel="noreferrer"
                           className="text-sm text-blue-500 hover:underline"
