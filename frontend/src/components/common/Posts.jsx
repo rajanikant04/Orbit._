a22a0a91f29b1e3feee32f15c4d479757c868225
Rajanikant04 <rajanikantsinghnitr@gmail.com>
2025-01-31 17:25:32 +0530

    the main issue will start

--- frontend/src/components/common/Posts.jsx
+++ frontend/src/components/common/Posts.jsx
@@ -1,9 +1,39 @@
 import Post from "./Post";
 import PostSkeleton from "../skeletons/PostSkeleton";
 import { POSTS } from "../../utils/db/dummy";
+import { useQuery } from "@tanstack/react-query"
 
-const Posts = () => {
-	const isLoading = false;
+const Posts = ({feedType}) => {
+
+	const getPostEndpoint = () => {
+		switch (feedType) {
+			case "forYou" :
+				return "/api/posts/all";
+			case "following":
+				return "/api/posts/following";
+			default: 
+				return "/api/posts/all";
+		}
+	}
+
+	const POST_ENDPOINT = getPostEndpoint();
+
+	const {data, isLoading} = useQuery({
+		queryKey: ["posts"],
+		queryFn: async() => {
+			try {
+				const res = await fetch(POST_ENDPOINT);
+				const data = await res.json();
+				if(!res.ok) {
+					throw new Error(data.error || "Something went wrong");
+				}
+
+				return data;
+			} catch (error) {
+				throw new Error(error);
+			}
+		}
+	})
 
 	return (
 		<>
